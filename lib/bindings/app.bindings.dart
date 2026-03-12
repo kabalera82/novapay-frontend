038db0096891c75da4872c6859b8f73dc91898e0
kabalera82 <kabalera@gmail.com>
2026-03-13 00:04:50 +0100

    bidding

---
+++ lib/bindings/app.bindings.dart
@@ -0,0 +1,20 @@
+// lib/bindings/app.bindings.dart
+import 'package:get/get.dart';
+import 'package:isar/isar.dart';
+import '../presentation/controllers/product.controller.dart';
+import '../presentation/controllers/ticket.controller.dart';
+import '../presentation/controllers/report.controller.dart';
+import '../presentation/controllers/config.controller.dart';
+
+class AppBindings extends Bindings {
+  final Isar isar;
+  AppBindings(this.isar);
+
+  @override
+  void dependencies() {
+    Get.put(ProductController(isar), permanent: true);
+    Get.put(TicketController(isar),  permanent: true);
+    Get.put(ReportController(isar),  permanent: true);
+    Get.put(ConfigController(isar),  permanent: true);
+  }
+}
