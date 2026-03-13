0ac59ff6720289b3f7c0179550d8581a85b9d712
Aarón Gómez <nomeacuerdobien@gmail.com>
2026-03-13 15:59:11 +0100

    full screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:isar/isar.dart';
 import 'package:media_kit/media_kit.dart';
+import 'package:flutter/services.dart'; // Para controlar la orientación y el modo de pantalla
 
 import 'config/theme.dart';
 import 'data/local/isar.dart';
@@ -16,6 +17,15 @@ import 'presentation/pages/admin/admin.shell.page.dart';
 import 'presentation/pages/profile.page.dart';
 
 void main() async {
+  // Asegura que los bindings de Flutter estén inicializados
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Oculta la barra de estado (arriba) y la barra de navegación/taskbar (abajo)
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+  // Fija la orientación en horizontal (Landscape) para que no se gire solo
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
   final isar = await openIsar();
@@ -37,10 +47,10 @@ class MainApp extends StatelessWidget {
       initialBinding: AppBindings(isar),
       home: const SplashPage(),
       routes: {
-        SplashPage.routename:      (_) => const SplashPage(),
-        LoginPage.routename:       (_) => const LoginPage(),
-        AdminShellPage.routename:  (_) => const AdminShellPage(),
-        ProfilePage.routename:     (_) => const ProfilePage(),
+        SplashPage.routename: (_) => const SplashPage(),
+        LoginPage.routename: (_) => const LoginPage(),
+        AdminShellPage.routename: (_) => const AdminShellPage(),
+        ProfilePage.routename: (_) => const ProfilePage(),
       },
       debugShowCheckedModeBanner: false,
     );
